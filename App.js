543d12adf961b6247ce60a28e2bb759bd0216537
Viktor Korotkiy <55664118+Armoteo@users.noreply.github.com>
2020-11-10 22:55:44 +0200

    fix

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import store from './src/store/index';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator, DrawerContentScrollView } from '@react-navigation/drawer';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Permissions, { PERMISSIONS } from 'react-native-permissions';
 import MainScreen from './src/screens/MainScreen';
 import RadioScreen from './src/screens/RadioScreens';
